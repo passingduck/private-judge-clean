7c24ba36c355b70409355614f14bf7ac182c8fc1
passingduck <hersungjin23@gmail.com>
2025-09-29 20:41:46 +0900

    Fix MESSAGES.COMMON.ERROR_OCCURRED type error

--- components/room/room-create.tsx
+++ components/room/room-create.tsx
@@ -126,7 +126,7 @@ export default function RoomCreate({
             className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
             role="alert"
           >
-            <strong className="font-bold">{MESSAGES.COMMON.ERROR_OCCURRED}</strong>
+            <strong className="font-bold">오류가 발생했습니다:</strong>
             <span className="block sm:inline ml-2">{error}</span>
           </div>
         )}
